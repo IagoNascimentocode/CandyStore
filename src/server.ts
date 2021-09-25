ee676e18d0c3986d03bb5d5d565baeceddd4c65a
IagoNascimentocode <iago_ns@hotmail.com>
2021-09-25 19:23:06 -0300

    handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
+import 'express-async-errors'
 import { router } from "./router";
 import "./modules/container";
 import "./database";
@@ -9,6 +10,14 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use((err:Error,request:Request,response:Response)=>{
 
+    if (err instanceof Error) {
+        return response.status(400).json({error:err.message})
+    }  
+
+    return response.status(500).json({status:"error", message:"Internal Server Error"})
+
+})
 
 app.listen(1024, () => console.log("Server running on the port:1024"));

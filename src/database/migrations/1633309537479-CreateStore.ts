a23b868d59714f2865474895b804a33eaffdafec
IagoNascimentocode <iago_ns@hotmail.com>
2021-10-08 16:39:33 -0300

    Sotre migration run

---
+++ src/database/migrations/1633309537479-CreateStore.ts
@@ -0,0 +1,57 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateStore1633309537479 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:"store",
+                columns:[
+                    {
+                        name:"id",
+                        type:"uuid",
+                        isPrimary:true
+                    },
+                    {
+                        name:"name",
+                        type:"varchar"
+                    },
+                    {
+                        name:"category",
+                        type:"varchar"
+                    },
+                    {
+                        name:"shelf_id",
+                        type:"uuid",
+                        isNullable:true
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ],
+                foreignKeys:[
+                    {
+                        name:"FKShelf",
+                        referencedTableName:"shelf",
+                        referencedColumnNames:["id"],
+                        columnNames:["shelf_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("store")
+    }
+
+}
---
+++ src/modules/store/entities/Store.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToMany, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import {v4 as uuid} from "uuid";
+import { Shelf } from "../../shelf/entities/Shelf";
+
+@Entity("store")
+class Store {
+    
+    @PrimaryColumn()
+    id:string;
+
+    @Column()
+    name:string;
+
+    @Column()
+    category:string;
+
+    @OneToMany(() => Shelf, shelf => shelf.id) 
+    @JoinColumn({name:"shelf_id"})
+    shelf:Shelf[];
+
+    @Column()
+    shelf_id:string;
+    
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor(){
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
+
+}
+export {Store}
